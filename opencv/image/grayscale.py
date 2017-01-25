5ed2a5b5df48ff35250c676a0ac8a84ce40dc34d
tanaka0079 <yadoran001@yahoo.co.jp>
2017-01-25 12:45:19 +0900

    Update grayscale.py

--- opencv/image/grayscale.py
+++ opencv/image/grayscale.py
@@ -2,8 +2,8 @@
 import cv2
 
 def main():
-    im = cv2.imread("test.jpg")                 # 画像を読み込み
-    gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)   # RGB画像をグレースケールに変換
+    img = cv2.imread("test.jpg")                  # 画像を読み込み
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # RGB画像をグレースケールに変換
     cv2.imshow("Show",gray)                       # ウィンドウの表示
     cv2.waitKey(0)
     cv2.destroyAllWindows()
